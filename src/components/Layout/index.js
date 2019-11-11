01ab0d341000f3aca624ea480c66d3c3e37548bd
guanghualiang <guanghualiang@steveliang-13mbp-0175.attlocal.net>
2019-11-10 19:53:35 -0800

    Adding author

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -17,7 +17,7 @@ const Layout = (props) => {
             <Container>
               <Content style={{ textAlign: 'center' }}>
                 <p>
-                  <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                  <strong>Ferro Rod Starter Kit</strong> by <a href="https://github.com/stliang">Steve Liang</a>. The source code is licensed
                   <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
                 is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                 </p>
