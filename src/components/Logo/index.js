37185ecd0b96f5d0540b482544808307e4735353
Steve Liang <stliang@gmail.com>
2020-03-21 22:57:17 -0700

    Fixed logo animation

---
+++ src/components/Logo/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Lottie from 'lottie-web-react'
+import Grid from '@material-ui/core/Grid';
+import * as demoConstants from './demoConstants'
+let watchAnimationData = require('./stopwatch.json')
+
+// https://stackoverflow.com/questions/58079525/how-to-show-page-after-animation-complete-with-lottie-and-javascript
+// https://www.freecodecamp.org/news/how-to-create-a-beautifully-animated-loader-in-react-native-21da37a8f6b0/
+// https://reactnative.dev/docs/modal#docsNav
+// https://programmingwithmosh.com/react/create-react-loading-spinner/
+
+function Logo() {
+    const [playingState, setPlayingState] = useState('play')
+    const [loopCounter, setLoopCounter] = useState(0)
+    const [autoplay, setAutoplay] = useState(false)
+    const [loop, setLoop] = useState(false)
+    const [path, setPath] = useState('https://assets2.lottiefiles.com/datafiles/zc3XRzudyWE36ZBJr7PIkkqq0PFIrIBgp4ojqShI/newAnimation.json')
+    const [animationData, setAnimationData] = useState(watchAnimationData)
+    const [speed, setSpeed] = useState(0.8)
+    const [direction, setDirection] = useState(1)
+
+    //Segment example variables
+    const [activeSegment, setActiveSegment] = useState([0, 10])
+
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}>
+            <Lottie
+                options={{
+                    renderer: demoConstants.renderer,
+                    loop: loop,
+                    autoplay: autoplay,
+                    path: path,
+                    animationData: animationData,
+                    rendererSettings: demoConstants.rendererSettings
+                }}
+                playingState={playingState}
+                speed={speed}
+                direction={direction}
+                eventListeners={[{
+                    eventName: 'complete',
+                    callback: () => { console.log('completed') }
+                }]}
+            />
+        </Grid>
+    )
+}
+
+export default Logo
--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ import UserContextProvider from './services/contexts/UserContextProvider';
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import Layout from './components/Layout';
 import POC from './scenes/POC';
+import UncontrolledLottie from './components/UncontrolledLottie'
+import Logo from './components/Logo';
 
 const App = props => (
     <FirebaseContextProvider>
         <UserContextProvider>
             <MemoryRouter>
-                <POC />
+                <Logo />
+                {/* <UncontrolledLottie /> */}
+                {/* <POC /> */}
                 {/* <Layout /> */}
             </MemoryRouter>
         </UserContextProvider>
