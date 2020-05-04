fb378b78bb7e7e0649768d4212c76acd41fca93c
Steve Liang <stliang@gmail.com>
2020-05-04 00:17:04 -0700

    remove unused files

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import * as serviceWorker from './serviceWorker';
 import FirebaseContextProvider from './services/contexts/FirebaseContextProvider';
 import UserContextProvider from './services/contexts/UserContextProvider';
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
-import Layout from './scenes/components/Layout';
 import Main from './scenes/Main';
-import Logo from './components/Logo';
 
 const App = props => (
     <FirebaseContextProvider>
         <UserContextProvider>
             <MemoryRouter>
-                {/* <Logo /> */}
                 <Main />
-                {/* <Layout /> */}
             </MemoryRouter>
         </UserContextProvider>
     </FirebaseContextProvider>
