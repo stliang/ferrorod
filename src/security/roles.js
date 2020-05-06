6543d169721600ffd8a7dcf8302f68b521837ae2
Steve Liang <stliang@gmail.com>
2020-05-05 20:04:39 -0700

    Clarify the meaning of exported roles

--- src/security/roles.js
+++ src/security/roles.js
@@ -1,3 +1,6 @@
+// A list of roles that can access a given exported role.
+// In the case of ANONYMOUS, all roles can access a page with
+// access privilige of ANONYMOUS role.
 export const ANONYMOUS   = ['anonymous', 'basic', 'paid', 'admin'];
 export const BASIC       = ['basic', 'paid', 'admin'];
 export const PAID        = ['paid', 'admin'];
